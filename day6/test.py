786c531a396897b783ae6d3bee8cb40a0dde4b20
Jerry <2655536160@qq.com>
2022-03-16 16:58:05 +0800

    0316study

--- day6/test.py
+++ day6/test.py
@@ -6,6 +6,8 @@
 class A(object):
     # 属性默认为类属性（可以给直接被类本身调用）
     num = "类属性"
+    # def __init__(self):
+    #     self.num1 = "初始化"
 
     # 实例化方法（必须实例化类之后才能被调用）
     def func1(self):  # self : 表示实例化类后的地址id
@@ -21,11 +23,11 @@ class A(object):
         cls().func1()
 
     # 不传递传递默认self参数的方法（该方法也是可以直接被类调用的，但是这样做不标准）
-    def func3():
-        print("func3")
-        print(A.num)  # 属性是可以直接用类本身调用的
+    # def func3():
+    #     print("func3")
+    #     print(A.num)  # 属性是可以直接用类本身调用的
 
 
 #A.func1() #这样调用是会报错：因为func1()调用时需要默认传递实例化类后的地址id参数，如果不实例化类是无法调用的
 A.func2()
-A.func3()
+#A.func3()
--- day6/初级版本/loginPage.py
+++ day6/初级版本/loginPage.py
@@ -3,3 +3,34 @@
 # @author   : Jerry
 # @datetime : 2022/3/15 19:14
 # @software : PyCharm
+
+from day6.初级版本.myDriver import Driver
+
+class LoginPage:
+    def __init__(self):
+        #创建driver对象
+        self.driver = Driver.getDriver()
+
+        #用户名输入框
+    def usernameBox(self):
+        return self.driver.find_element_by_name("username")
+
+        #密码输入框
+    def pwdBox(self):
+        return self.driver.find_element_by_name("password")
+
+        #登录按钮
+    def loginButton(self):
+        return self.driver.find_element_by_css_selector("button.btn")
+
+#抽离出页面动作类，继承相应的页面类
+class LoginPageAction(LoginPage):
+    def login(self):
+        """登录系统"""
+        self.usernameBox().send_keys("libai")
+        self.pwdBox().send_keys("opmsopms123")
+        self.loginButton().click()
+
+if __name__ == '__main__':
+    lp = LoginPageAction()
+    lp.login()
